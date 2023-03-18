fb29367c1842496e89b1007b83def3b818557ec2
sam21nw <91039661+sam21nw@users.noreply.github.com>
2023-03-18 09:20:46 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -6,3 +6,23 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
 console.log('hello');
+
+type Employee = {
+  name?: string;
+  active: boolean;
+  stack: string[];
+};
+
+let emp1: Employee = {
+  active: false,
+  stack: ['JS', 'TS', 'CSS'],
+};
+
+function greetEmployee(employee: Employee): string {
+  if (employee.name) {
+    return `Hello ${employee.name}`;
+  }
+  return `Hello Dude`;
+}
+
+console.log(greetEmployee(emp1));
